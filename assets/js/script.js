9a27d5d69109bd4aa538005e00fa757feb732cb8
Enis Hoxha <enishoxha10@icloud.com>
2023-01-20 23:40:06 +0100

    Project completion

--- assets/js/script.js
+++ assets/js/script.js
@@ -89,38 +89,38 @@
     });
     
     
-    // Related News Slider
-    $('.sn-slider').slick({
-        autoplay: false,
-        infinite: true,
-        dots: false,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        responsive: [
-            {
-                breakpoint: 1200,
-                settings: {
-                    slidesToShow: 3
-                }
-            },
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 3
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2
-                }
-            },
-            {
-                breakpoint: 576,
-                settings: {
-                    slidesToShow: 1
-                }
-            }
-        ]
-    });
+    // // Related News Slider
+    // $('.sn-slider').slick({
+    //     autoplay: false,
+    //     infinite: true,
+    //     dots: false,
+    //     slidesToShow: 3,
+    //     slidesToScroll: 1,
+    //     responsive: [
+    //         {
+    //             breakpoint: 1200,
+    //             settings: {
+    //                 slidesToShow: 3
+    //             }
+    //         },
+    //         {
+    //             breakpoint: 992,
+    //             settings: {
+    //                 slidesToShow: 3
+    //             }
+    //         },
+    //         {
+    //             breakpoint: 768,
+    //             settings: {
+    //                 slidesToShow: 2
+    //             }
+    //         },
+    //         {
+    //             breakpoint: 576,
+    //             settings: {
+    //                 slidesToShow: 1
+    //             }
+    //         }
+    //     ]
+    // });
 })(jQuery);
